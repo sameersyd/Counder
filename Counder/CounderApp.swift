28785808a55066475659c152deab33f882d89bf1
Sameer Nawaz <sameer.nwaz@gmail.com>
2021-03-05 13:20:51 +0530

    Initial Commit

---
+++ Counder/CounderApp.swift
@@ -0,0 +1,17 @@
+//
+//  CounderApp.swift
+//  Counder
+//
+//  Created by Sameer Nawaz on 05/03/21.
+//
+
+import SwiftUI
+
+@main
+struct CounderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
